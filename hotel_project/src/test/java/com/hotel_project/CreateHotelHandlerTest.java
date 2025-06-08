b5075606055b9f5c44498f9c08151fac20a6e750
Timazay <oxxxymofey@gmail.com>
2025-06-08 16:08:09 +0300

    Update CreateHotelHandlerTest.java

--- hotel_project/src/test/java/com/hotel_project/CreateHotelHandlerTest.java
+++ hotel_project/src/test/java/com/hotel_project/CreateHotelHandlerTest.java
@@ -18,9 +18,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
